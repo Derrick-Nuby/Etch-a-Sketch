47a38ee52703f865b7182c2ec51cd4bb18ae104e
Derrick-Nuby <iradukundaderrick7@gmail.com>
2023-07-19 11:32:38 +0200

    decided to use html to add stuff

---
+++ app.js
@@ -0,0 +1,18 @@
+body = document.querySelector("body");
+
+function updateSizeValue(value) {
+  document.getElementById("sizeValue").innerText = value + " X " + value;
+}
+// title = document.createElement("h1");
+// title.textContent = "Sketch Paper Website | Etch a Sketch";
+// body.appendChild(title);
+
+// navigation = document.createElement("div");
+// navigation.classList.add("navigation");
+// navigation.textContent = "Hello world";
+// body.appendChild(navigation);
+
+// container = document.createElement("div");
+// container.classList.add("container");
+// container.textContent = "Hello world";
+// body.appendChild(container);
